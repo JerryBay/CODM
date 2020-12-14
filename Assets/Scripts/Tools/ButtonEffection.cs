4604c77dc26717cdbd84f778e46bd8bc90a47877
jerryjcbai <1018679568@qq.com>
2020-12-14 17:08:00 +0800

    button effect

---
+++ Assets/Scripts/Tools/ButtonEffection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonEffection : MonoBehaviour
+{
+    public Button[] _buttons;
+    private void Awake()
+    {
+        _buttons = GetComponentsInChildren<Button>();
+        for (int i = 0; i < _buttons.Length; i++)
+        {
+            _buttons[i].onClick.AddListener(ChangeColor);
+        }
+    }
+
+    private void ChangeColor()
+    {
+        for (int j = 0; j < _buttons.Length; j++)
+        {
+            _buttons[j].GetComponent<RawImage>().color = new Color(1, 1, 1, 1);
+        }
+        GameObject button = EventSystem.current.currentSelectedGameObject;
+        button.GetComponent<RawImage>().color = new Color(0.5f, 0.5f, 0.5f, 1);
+    }
+}
--- Assets/Scripts/Tools/MouseMoveToRotate.cs
+++ Assets/Scripts/Tools/MouseMoveToRotate.cs
@@ -15,11 +15,23 @@ public class MouseMoveToRotate : MonoBehaviour
     private const int _orientationControlButton= 1;
     private const int _pedestalControlButton = 2;
 
+    private Vector3 _distance;
+    public Camera _camera;
+    public float _minDist;
+    public float _maxDist;
+
+    [Range(0.5f,5f)]
+    public float ScrollSensitivity=1f;
+
     private void Start()
     {
         _target = _characterManager._currentCharacter.transform;
+        _distance = _target.localPosition - _camera.transform.localPosition;
         _characterManager.AddSubscriber(
-            (instance)=>ChangeCharacter(_characterManager._currentCharacter)
+            (instance)=> 
+            { 
+                ChangeCharacter(_characterManager._currentCharacter);
+            }
             );
     }
 
@@ -28,9 +40,12 @@ public class MouseMoveToRotate : MonoBehaviour
         _target = cha.transform;
     }
 
+
     private void LateUpdate()
     {
         Vector2 mouseDelta;
+        Transform cameraTr = _camera.transform;
+        _distance = _target.localPosition - _camera.transform.localPosition;
 
         if (Input.GetMouseButtonDown(_orientationControlButton))
         {
@@ -43,7 +58,15 @@ public class MouseMoveToRotate : MonoBehaviour
             _target.Rotate(0, -mouseDelta.x * _rotateSpeed * Time.deltaTime, 0);
         }
 
+        float moveDist = _distance.magnitude;
+
+        float ratio = moveDist / (_maxDist - _minDist);
+
+
+        cameraTr.Translate(Input.mouseScrollDelta.y * _distance * ratio * Time.deltaTime * ScrollSensitivity);
+
         _preMousePos = _nowMousePos;
+
     }
 }
 
